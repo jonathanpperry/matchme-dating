4984612f2d63236509264c67fa5ae5a921b15b83
Jonathan Perry <jpper@umich.edu>
2024-11-12 12:23:13 -0500

    Create login and register forms with validation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <TopNav />
-        <Providers>{children}</Providers>
+        <Providers>
+          <TopNav />
+          <main className="container mx-auto">{children}</main>
+        </Providers>
       </body>
     </html>
   );
