bda61b84ee125ed15124938d8377e2838202a4b8
Jonathan Perry <jpper@umich.edu>
2024-11-12 18:09:40 -0500

    Built like feature with prisma relations

--- src/app/lists/page.tsx
+++ src/app/lists/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import {
+  fetchCurrentUserLikeIds,
+  fetchLikedMembers,
+} from "../actions/likeActions";
+import ListsTab from "./ListsTab";
 
-const ListPage = () => {
+export default async function ListsPage({
+  searchParams,
+}: {
+  searchParams: { type: string };
+}) {
+  const likeIds = await fetchCurrentUserLikeIds();
+  const members = await fetchLikedMembers(searchParams.type);
   return (
-    <div>ListPage</div>
-  )
+    <div>
+      <ListsTab members={members} likeIds={likeIds} />
+    </div>
+  );
 }
-
-export default ListPage
